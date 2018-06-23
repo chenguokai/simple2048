93d31943dc353abd5d4ce7b3f40b95ff11e50cab
chenguokai <1415430544@qq.com>
2018-06-23 23:47:27 +0800

    Initial Commit

---
+++ Swift-Bomb-2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift-Bomb-2048
+//
+//  Created by Xim on 2018/6/23.
+//  Copyright © 2018年 chenguokai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
